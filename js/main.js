67a8787345f2b98d329b80c395d5d35e3bc08f9e
Adriana Castillo <adricasra1993@gmail.com>
2025-09-28 17:03:20 -0500

    Se creo la lista de vehiculos, se agrego la función para agregar al carrito, se creo la lista de vehiculos del carrito

--- js/main.js
+++ js/main.js
@@ -5,7 +5,15 @@ let marca = '';
 let modelo = '';
 let kilometraje = '';
 let precio = '';
-let contCarros = document.getElementById('cont-list-cars')
+let contCarros = document.getElementById('cont-list-cars');
+
+const btnCar = document.getElementById('button-carrito');
+const contOculto = document.getElementById('cont-carrito');
+let contTotalCarrito = document.getElementById('total-carrito');
+let totalCarrito = 0;
+
+let contCarrito = document.getElementById('cont-products');
+
 
 function addCard() {
     // Construimos la tarjeta
@@ -53,8 +61,6 @@ function addCard() {
     tituloPrecio.classList.add('precio-vehiculo');
     tituloPrecio.textContent = '$' + precio;
 
-
-
     const buttons = document.createElement('div')
     buttons.classList.add('d-flex', 'justify-content-between', 'mt-3');
 
@@ -81,7 +87,6 @@ function addCard() {
 
     // creamos el retun para dar respuesta del elemento crado ya que lo usaremos en otra function mas adelante
     return item;
-
 }
 
 // Se captura el valor de los elementos del formulario con el submit
@@ -111,23 +116,128 @@ form.addEventListener('submit', (e) => {
     const newItem = addCard();
     contCarros.appendChild(newItem);
     eventsToItem(newItem);
-
 });
 
+// Función para pintar producto en el carrito 
+function addProductCar(item) {
+    foto_carrito = item.querySelector('.card-img-top').src;
+    nombre_carrito = item.querySelector('.card-title').textContent;
+    marca_carrito = item.querySelector('.card-subtitle').textContent;
+    precio_carrito = item.querySelector('.text-success').textContent;
 
+    const nombre_marca = marca_carrito.split(':');
+    const precioReal = precio_carrito.split('$');
+    const newCartItem = addToCart(foto_carrito, nombre_carrito, nombre_marca[1].trim(), precioReal[1].trim());
 
+    contCarrito.appendChild(newCartItem);
+    actualizarTotal(precioReal[1].trim());
+}
 
+// Function eventos de articulo
 function eventsToItem(item) {
 
     const comprar = item.querySelector('.b-comprar');
     comprar.addEventListener('click', () => {
+        addProductCar(item);
         alert('Compra con exito');
     })
-
 
     const deleteBtn = item.querySelector(".b-eliminar");
     deleteBtn.addEventListener('click', () => {
         item.remove();
     })
 }
+
+function setCarToList(foto_carrito, nombre_carro, marca_carro, precio_carro) {
+    const listCarros = JSON.parse(localStorage.getItem('carros')) || [];
+    listCarros.push({
+        foto: foto_carrito,
+        nombre: nombre_carro,
+        marca: marca_carro,
+        precio: precio_carro
+    });
+
+    localStorage.setItem('carros', JSON.stringify(listCarros));
+}
+
+function deleteCarToList(nombre_carro, marca_carro) {
+    const listCarros = JSON.parse(localStorage.getItem('carros')) || [];
+    
+    if (listCarros.length > 0) {
+        const indexCarroEliminar = listCarros.findIndex(objeto => objeto.nombre === nombre_carro && objeto.marca === marca_carro);
+        // Actualizamos el valor del carrito
+        totalCarrito = totalCarrito - Number(listCarros[indexCarroEliminar].precio);
+        contTotalCarrito.textContent = 'Total: $ ' + totalCarrito;
+
+        // Eliminamos el vehiculo del Carrito
+        listCarros.splice(indexCarroEliminar, 1);
+        localStorage.setItem('carros', JSON.stringify(listCarros));
+    }
+}
+
+
+function addToCart(foto_carro, nombre_carro, marca_carro, precio_carro) {
+    setCarToList(foto_carro, nombre_carro, marca_carro, precio_carro);
+    const item = document.createElement('div');
+    item.classList.add('col-md-6', 'item-vehiculo-carrito');
+
+    const card = document.createElement('div');
+    card.classList.add('card', 'h-100');
+
+    const img = document.createElement('img');
+    img.setAttribute('src', foto_carro);
+    img.classList.add('card-img-top');
+    img.classList.add('w-100');
+    img.setAttribute('alt', 'Foto vehículo');
+
+    card.appendChild(img);
+
+    const carBody = document.createElement('div');
+    carBody.classList.add('card-body');
+
+    const tituloNombre = document.createElement('h3');
+    tituloNombre.classList.add('card-title');
+    tituloNombre.textContent = nombre_carro;
+
+    const tituloMarca = document.createElement('h4');
+    tituloMarca.classList.add('card-subtitle', 'text-muted');
+    tituloMarca.textContent = 'Marca: ' + marca_carro;
+
+    const tituloPrecio = document.createElement('h2');
+    tituloPrecio.classList.add('text-success', 'precio-vehiculo');
+    tituloPrecio.textContent = '$' + precio_carro;
+
+    const buttons = document.createElement('div')
+    buttons.classList.add('d-flex', 'justify-content-between', 'mt-3');
+
+    const btnEliminar = document.createElement('button');
+    btnEliminar.classList.add('btn', 'btn-danger', 'b-eliminar');
+    btnEliminar.textContent = 'Eliminar';
+
+    // Agregamos los nodos de la tarjeta de información del vehiculo
+    carBody.appendChild(tituloNombre);
+    carBody.appendChild(tituloMarca);
+    carBody.appendChild(tituloPrecio);
+    carBody.appendChild(btnEliminar);
+
+    btnEliminar.addEventListener('click', () => {
+        item.remove();
+        deleteCarToList(nombre_carro, marca_carro)
+    })
+
+
+    // Ensamblamos dentro del nodo padre sus nodos hijos, es decir la estructura de la tarea
+    card.appendChild(carBody);
+    item.appendChild(card);
+
+    // creamos el retun para dar respuesta del elemento crado ya que lo usaremos en otra function mas adelante
+    return item;
+}
+
+function actualizarTotal(precio) {
+    const valor = precio.trim().replace(/[^0-9.-]+/g, "");
+    totalCarrito += Number(valor);
+    contTotalCarrito.textContent = 'Total: $ ' + totalCarrito;
+}
+
 
